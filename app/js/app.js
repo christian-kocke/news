77b1f700c9d48b45f90f91dd96a874e6fe5da24c
nonov <noe.viricel@orange.fr>
2015-04-07 11:55:20 +0700

    Add User Profil Page

--- app/js/app.js
+++ app/js/app.js
@@ -6,27 +6,29 @@ var newsApp = angular.module('newsApp', [
   'ngRoute',
   'newsControllers',
   'newsServices'
-]);
+  ]);
 
 newsApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/login', {
-        templateUrl: 'partials/login.html',
-        controller: 'AuthCtrl'
-      }).
-      when('/admin', {
-        templateUrl: 'partials/admin.html'
-      }).
-      when('/', {
-        templateUrl: 'partials/registration.html'
-      }).
-      when('/client', {
-        templateUrl: 'partials/client-news-feed.html',
-        controller: 'NewsCtrl'
-      }).
-      
-      otherwise({
-        redirectTo: '/'
-      });
+    when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'AuthCtrl'
+    }).
+    when('/admin', {
+      templateUrl: 'partials/admin.html'
+    }).
+    when('/', {
+      templateUrl: 'partials/registration.html'
+    }).
+    when('/profil', {
+      templateUrl: 'partials/userProfil.html'
+    }).
+    when('/client', {
+      templateUrl: 'partials/client-news-feed.html',
+      controller: 'NewsCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
   }]);
