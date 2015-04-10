b6517a2ee20cb54454063b1b9e51d0f486434f71
nonov <noe.viricel@orange.fr>
2015-04-10 17:37:54 +0700

    Design Rebuilded

--- app/js/upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-	header('content-type: application/json');
-
-	$source = file_get_contents('php://input');
-	$filename = $_FILES['file']['name'];
-	echo json_encode(move_uploaded_file( $_FILES['file']['tmp_name'] , '../imgDrop/user'.$id ));
-?>
